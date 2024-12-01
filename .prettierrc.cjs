39b8131f45a63e6110b167dc8775c5e163bbeb48
Carol Li <dinosaur0514@gmail.com>
2024-12-01 15:26:05 +0800

    initial commit

---
+++ .prettierrc.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  eslintInter: true,
+  tabWidth: 2,
+  semi: true,
+  trailingComma: "all",
+  bracketSpacing: true, // 大括號内的首尾加上空格
+  arrowParens: "always", // 箭頭函式只有一個參數的時候也需要括號
+  rangeStart: 0, // 文件格式化的範圍是文件的全部内容
+  rangeEnd: Infinity, // 文件格式化的範圍是文件的全部内容
+  endOfLine: "lf", // 換行符使用 lf
+};
