19d204e8fe7f044ef0d0e768079902340888b9b4
Carol Li <dinosaur0514@gmail.com>
2024-12-01 21:42:57 +0800

    style: 新增按鈕樣式
    有 Normal Order、VIP Order、新增 Bot 及移除 Bot 按鈕

--- src/components/Control.jsx
+++ src/components/Control.jsx
@@ -1,7 +1,30 @@
 export default function Control() {
+  let defaultButtonClasses =
+    "inline-flex algin-items-center justify-content-center px-3 py-2 rounded-md text-sm";
+
   return (
-    <div className="rounded-md border p-4">
-      <h3 className="font-bold">Control</h3>
+    <div className="flex flex-row flex-wrap space-x-2 rounded-md border p-4">
+      <button
+        className={`${defaultButtonClasses} text-white bg-blue-500 hover:bg-blue-600`}
+      >
+        + Normal Order
+      </button>
+
+      <button
+        className={`${defaultButtonClasses} text-white bg-orange-500 hover:bg-orange-600`}
+      >
+        + VIP Order
+      </button>
+      <button
+        className={`${defaultButtonClasses} text-blue-500 bg-white border border-blue-500 hover:text-white hover:bg-blue-600 hover:border-blue-600`}
+      >
+        + Bot
+      </button>
+      <button
+        className={`${defaultButtonClasses} text-red-500 bg-white border border-red-500 hover:text-white hover:bg-red-600 hover:border-red-600`}
+      >
+        - Bot
+      </button>
     </div>
   );
 }
