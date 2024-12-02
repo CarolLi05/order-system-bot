a63aebeaf48a4015cbdc9911d1d11bf3a06e8a57
Carol Li <dinosaur0514@gmail.com>
2024-12-02 22:04:13 +0800

    feat: 新增 Bot 元件

---
+++ src/components/Bot.jsx
@@ -0,0 +1,10 @@
+export default function Bot() {
+  let BotClasses =
+    "inline-flex flex-wrap algin-items-center justify-content-center px-2 py-1 rounded-full text-sm text-white bg-gray-400";
+
+  return (
+    <li className={BotClasses}>
+      Bot #1 IDLE
+    </li>
+  );
+}
